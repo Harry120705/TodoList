a01733704863205614fd4207f04bc633574bf62f
Harrison <hahcarvalho@gmail.com>
2025-01-19 17:56:17 -0300

    Alterações de leitura e organização de ordem de elementos

--- todolist/todolist/src/main/java/br/com/harrisoncarvalho/todolist/task/TaskModel.java
+++ todolist/todolist/src/main/java/br/com/harrisoncarvalho/todolist/task/TaskModel.java
@@ -29,10 +29,13 @@ public class TaskModel {
     @Id
     @GeneratedValue(generator = "UUID")
      private UUID id;
-     private String description;
 
      @Column(length= 50)
      private String title;
+     
+     private String description;
+
+     
      private LocalDateTime startAt;
      private LocalDateTime endAt;
      private String priority;
